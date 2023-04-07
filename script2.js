9e4923996596639da71760fc3cf10fceac02e4bf
bboulay <bboulay@users.noreply.github.com>
2023-04-07 13:48:03 +0300

    Create script2.js

---
+++ script2.js
@@ -0,0 +1,55 @@
+function staticLoadPlaces() {
+    return [
+        {
+            name: 'object1',
+            location: {
+                lat: 60.150917,
+                lng: 24.638699,
+            },
+        },
+       {
+            name: 'object2',
+            location: {
+                lat: 60.151035,
+                lng: 24.638924,
+            },
+        },
+    ];
+}
+
+
+window.onload = () => {
+    const scene = document.querySelector('a-scene');
+
+    // first get current user location
+    return navigator.geolocation.getCurrentPosition(function (position) {
+
+        // than use it to load from remote APIs some places nearby
+        staticLoadPlaces()
+            .then((places) => {
+                places.forEach((place) => {
+                    const latitude = place.location.lat;
+                    const longitude = place.location.lng;
+
+                    // add place name
+                    const placeText = document.createElement('a-link');
+                    placeText.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+                    placeText.setAttribute('title', place.name);
+                    placeText.setAttribute('scale', '15 15 15');
+                    
+                    placeText.addEventListener('loaded', () => {
+                        window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
+                    });
+
+                    scene.appendChild(placeText);
+                });
+            })
+    },
+        (err) => console.error('Error in retrieving position', err),
+        {
+            enableHighAccuracy: true,
+            maximumAge: 0,
+            timeout: 27000,
+        }
+    );
+};
