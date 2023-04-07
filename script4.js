b0f324bb2e90317437a6ad016e48f0877a308243
bboulay <bboulay@users.noreply.github.com>
2023-04-07 18:36:42 +0300

    Update script4.js

--- script4.js
+++ script4.js
@@ -2,12 +2,12 @@ var objects = {};
 objects['object5'] = {
         name: 'object5', 
         description: 'long description for the object 5, that can take several lines and even more', 
-        description_image: 'image/object5.png'
+        description_image: './5.png'
 };
 objects['object6'] = {
         name: 'object6', 
         description: 'long description for the object 6, that can take several lines and even more', 
-        description_image: 'image/object6.png'
+        description_image: './06-barcode.png'
 };
         
 
@@ -23,6 +23,9 @@ AFRAME.registerComponent('markerhandler', {
       marker.addEventListener('markerFound', () => {
         div.innerText = objects[marker.id].name;
         description.innerText = objects[marker.id].description;
+        var elem = document.createElement("img");
+        elem.src = objects[marker.id].description_image;
+        div.appendChild("elem")
         //description.innerText = objects[marker.id].description;
         //description_image.innerText = objects[marker.id].description_image;
       }),
